e16f5e77e723a9429f7931aaa710e1efa7c243c6
VinayakGardi <vinayakgardi7@gmail.com>
2022-12-25 08:54:49 +0530

    made it lucky number game using dice rolling and added few widgets

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll)
         val diceAnswer : TextView = findViewById(R.id.textView)
+        val Message : TextView =findViewById(R.id.Message)
 
 
         rollButton.setOnClickListener {
-            diceAnswer.text = (1..6).random().toString()
-            Toast.makeText(this, "Changed Number" , Toast.LENGTH_LONG).show()
+           val luckNumber : String= (1..6).random().toString()
+           diceAnswer.text=(1..6).random().toString()
+            if(diceAnswer.text == luckNumber)
+            {
+                Message.setText("Got it !! \uD83E\uDD73")
+            }
+            else
+            {
+               Message.setText("Try again !!\uD83D\uDE15")
+            }
+
         }
 
     }
